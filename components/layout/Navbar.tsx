19e598606db9115faffd7dead7952713c3eb5379
logan-anderson <landerson2@upei.ca>
2020-05-31 12:43:46 -0300

    centered text

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
+import NavItem from './NavItem'
 
 const navBar = () => {
   return (
     <nav className="flex flex-wrap items-center justify-between p-4">
-      <div className="lg:order-2 w-auto lg:w-1/5 lg:text-center">
-        <Link href="#">
-          <div className="text-xl text-indigo-500 font-semibold">
-            Dunder Mifflin
-          </div>
-        </Link>
+      <div className="lg:order-2 w-auto lg:w-1/3 lg:text-center">
+        <div className="text-xl text-indigo-500 font-semibold">
+          {/* main nav */}
+          {/* <Link href="/"> */}
+              Home
+          {/* </a> */}
+          {/* </Link> */}
+        </div>
       </div>
+      {/* mobile button */}
       <div className="block lg:hidden">
         <button className="navbar-burger flex items-center py-2 px-3 text-indigo-500 rounded border border-indigo-500">
           <svg
@@ -21,46 +25,18 @@ const navBar = () => {
           </svg>
         </button>
       </div>
-      <div className="navbar-menu hidden lg:order-1 lg:block w-full lg:w-2/5">
-        <a
-          className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-blue-700"
-          href="#"
-        >
-          Products
-        </a>
-        <a
-          className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-blue-700"
-          href="#"
-        >
-          Team
-        </a>
-        <a
-          className="block lg:inline-block mt-4 lg:mt-0 text-blue-900 hover:text-blue-700"
-          href="#"
-        >
-          Customers
-        </a>
+      {/* left side */}
+      <div className="navbar-menu hidden lg:order-1 lg:block w-full lg:w-1/3 lg:text-left">
+      <a
+        href="https://logan.bio"
+        className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-blue-700"
+      >
+        More about me
+         </a>
       </div>
-      <div className="navbar-menu hidden lg:order-3 lg:block w-full lg:w-2/5 lg:text-right">
-        <Link href='/blog'>
-          <a
-            className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-blue-700"
-          >
-            All Blog Posts
-          </a>
-        </Link>
-        <a
-          className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-blue-700"
-          href="#"
-        >
-          FAQ
-        </a>
-        <a
-          className="block lg:inline-block mt-4 lg:mt-0 text-blue-900 hover:text-blue-700"
-          href="#"
-        >
-          Contact
-        </a>
+      <div className="navbar-menu hidden lg:order-3 lg:block w-full lg:w-1/3 lg:text-right">
+        <NavItem name="All blogs" link="/blog" />
+
       </div>
     </nav>
   );
