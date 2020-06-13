6282ade168628373247421da6d789515e448dca0
Logan Anderson <landerson2@upei.ca>
2020-06-12 22:31:12 -0300

    added sitemap

--- sitemapgen.js
+++ sitemapgen.js
@@ -1,19 +1,22 @@
 const sitemap = require("nextjs-sitemap-generator");
 const fs = require("fs");
 const BUILD_ID = fs.readFileSync(".next/BUILD_ID").toString();
-sitemap({
-    baseUrl: "https://logana.dev",
-    ignoredPaths: ['[slug]', 'api', 'github'],
-    pagesDirectory: __dirname + "/.next/server/static/" + BUILD_ID + "/pages",
-    targetDirectory: "public/",
-    extraPaths: ['/'],
-    pagesConfig: {
-        '/': {
-            priority: '1',
+(async function() {
+    await Promise.resolve(sitemap({
+        baseUrl: "https://logana.dev",
+        ignoredPaths: ['[slug]', 'api', 'github'],
+        pagesDirectory: __dirname + "/.next/server/static/" + BUILD_ID + "/pages",
+        targetDirectory: "public/",
+        extraPaths: ['/'],
+        pagesConfig: {
+            '/': {
+                priority: '1',
+            },
+            '/blog': {
+                priority: '0.5',
+                changefreq: 'daily'
+            }
         },
-        '/blog': {
-            priority: '0.5',
-            changefreq: 'daily'
-        }
-    },
-})
+    }));
+    // → 🎉
+  }());
