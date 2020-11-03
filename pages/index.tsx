f056a83680e1c7641fb9382dc38fce32b179d81c
logan-anderson <landerson2@upei.ca>
2020-11-03 16:55:16 -0400

    removed double text

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,9 +36,6 @@ const IndexPage = ({ file, preview, posts }: props) => {
       <Layout title="Home" preview={preview} navDisable={true}>
         <Hero />
         <section className="py-12 px-4">
-          <h2 className="text-3xl text-center mb-8 font-heading">
-            {data.posts_title}
-          </h2>
 
           <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
             <div className="absolute inset-0">
@@ -47,11 +44,10 @@ const IndexPage = ({ file, preview, posts }: props) => {
             <div className="relative max-w-7xl mx-auto">
               <div className="text-center">
                 <h2 className="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10">
-                  Latest Blog posts
+                {data.posts_title}
                 </h2>
                 <p className="mt-3 max-w-2xl mx-auto text-xl leading-7 text-gray-500 sm:mt-4">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
-                  libero labore natus atque, ducimus sed.
+                {/* {sub title here} */}
                 </p>
               </div>
               <div className="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-3 lg:max-w-none">
