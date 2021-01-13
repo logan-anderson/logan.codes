08b2d04b5cc02cd60e2d7bc67d68779c50ddfd8d
Logan Anderson <landerson2@upei.ca>
2021-01-12 20:04:27 -0400

    updated resume url

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import Layout from "../components/layout/Layout";
 const RESUME_URL =
-  "https://docs.google.com/document/d/1vEcbGnN0f1MGhA5WH0Gmt_h5Cvxf7l_xmtT8Ao1Mda4/edit";
+  "https://docs.google.com/document/d/16QJ6ue2xNkPFQO6nVeTuccWsxeG-6km8CD6VHDQCb0w/edit?usp=sharing";
 
 const ResumePage = () => {
   useEffect(() => {
