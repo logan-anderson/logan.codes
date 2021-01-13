4076190674b497509a2197b3acabe466da75f36d
Logan Anderson <landerson2@upei.ca>
2021-01-12 21:53:20 -0400

    added files

---
+++ components/ProjectCard.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import React from "react";
+import { Projects } from "../pages/projects";
+
+export const ProjectCard: React.FC<{ project: Projects }> = ({ project }) => {
+  const hasImg = Boolean(project.imgUrl);
+  return (
+    <Link href={`/projects/${project.slug}`}>
+      <div className="transition duration-500 ease-in-out transform hover:-translate-y-1/2 hover:scale-103 hover:underline cursor-pointer color-blue">
+        <div className={`grid ${hasImg && "grid-cols-2"} gap-5`}>
+          <div>
+            <h2>{project.label}</h2>
+            <div>{project.excerpt}</div>
+          </div>
+          <div>
+            {project.imgUrl && <img src={project.imgUrl} alt="image" />}
+          </div>
+        </div>
+
+        {/* divider */}
+        <div className="relative">
+          <div
+            className="absolute inset-0 flex items-center"
+            aria-hidden="true"
+          >
+            <div className="w-full border-t border-gray-300"></div>
+          </div>
+          <div className="relative flex justify-center">
+            <span className="px-3 bg-white text-sm text-gray-500 my-auto">
+              See more{" "}
+              <span
+                className="text-2xl"
+                style={{
+                  display: "inline-block",
+                  verticalAlign: "middle",
+                }}
+              >
+                &#8680;
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
