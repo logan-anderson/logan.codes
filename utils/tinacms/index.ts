ae2a87c7837852f35eff53284a8484bff5c264c9
Logan Anderson <landerson2@upei.ca>
2021-05-04 09:17:01 -0300

    added file'

---
+++ utils/tinacms/index.ts
@@ -0,0 +1,16 @@
+import { MarkdownFieldPlugin } from "react-tinacms-editor";
+import { DateFieldPlugin } from "react-tinacms-date";
+import { useCMS, usePlugins } from "tinacms";
+import { useEditState } from "../editState";
+
+export const useSetupPlugins = () => {
+  usePlugins([MarkdownFieldPlugin, DateFieldPlugin]);
+};
+
+export const useExitTina = () => {
+  const cms = useCMS();
+  const { setEdit } = useEditState();
+  cms.events.subscribe("cms:disable", (args) => {
+    setEdit(false);
+  });
+};
