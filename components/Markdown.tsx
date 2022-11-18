a4f038477c210da65a60eb2a8a6275d0a140ce39
Logan Anderson <landerson2@upei.ca>
2022-11-18 11:03:55 -0500

    remove deps

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,46 +1,46 @@
-import React from "react";
-import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// import React from "react";
+// import ReactMarkdown from "react-markdown";
+// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 // @ts-ignore
-import { okaidia as Theme } from "react-syntax-highlighter/dist/cjs/styles/prism";
+// import { okaidia as Theme } from "react-syntax-highlighter/dist/cjs/styles/prism";
 export const STYLES =
   "prose prose-blue prose-lg text-lg max-w-prose mx-auto mb-6 dark:prose-dark";
-export const MarkdownBody: React.FC<ReactMarkdown.ReactMarkdownProps> = (
-  props
-) => {
-  return (
-    // @ts-ignore
-    <ReactMarkdown
-      renderers={{
-        heading: ({ children, level }) => {
-          if (level === 1) {
-            return (
-              <h1 className="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 dark:text-gray-50">
-                {children}
-              </h1>
-            );
-          } else {
-            return (
-              <h2 className="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 dark:text-gray-50">
-                {children}
-              </h2>
-            );
-          }
-        },
-        paragraph: ({ children }) => {
-          return <p className="text-gray-500 mx-auto">{children}</p>;
-        },
-        code: ({ language, value }) => {
-          return (
-            <SyntaxHighlighter
-              style={Theme}
-              language={language}
-              children={value}
-            />
-          );
-        },
-      }}
-      {...props}
-    />
-  );
-};
+// export const MarkdownBody: React.FC<ReactMarkdown.ReactMarkdownProps> = (
+//   props
+// ) => {
+//   return (
+//     // @ts-ignore
+//     <ReactMarkdown
+//       renderers={{
+//         heading: ({ children, level }) => {
+//           if (level === 1) {
+//             return (
+//               <h1 className="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 dark:text-gray-50">
+//                 {children}
+//               </h1>
+//             );
+//           } else {
+//             return (
+//               <h2 className="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 dark:text-gray-50">
+//                 {children}
+//               </h2>
+//             );
+//           }
+//         },
+//         paragraph: ({ children }) => {
+//           return <p className="text-gray-500 mx-auto">{children}</p>;
+//         },
+//         code: ({ language, value }) => {
+//           return (
+//             <SyntaxHighlighter
+//               style={Theme}
+//               language={language}
+//               children={value}
+//             />
+//           );
+//         },
+//       }}
+//       {...props}
+//     />
+//   );
+// };
