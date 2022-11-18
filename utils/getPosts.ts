128f381e8076a5f3b0a44810706148401fa7e2dd
Logan Anderson <landerson2@upei.ca>
2022-11-18 13:41:10 -0500

    dont use preview

--- utils/getPosts.ts
+++ utils/getPosts.ts
@@ -2,9 +2,8 @@ import { client } from "../.tina/__generated__/client";
 import type { PostFilter } from "../.tina/__generated__/types";
 
 export const getPosts = async () => {
-  const showDrafts =
-    Boolean(process.env.NEXT_PUBLIC_SHOW_DRAFTS || "") ||
-    process.env.VERCEL_ENV === "preview";
+  const showDrafts = Boolean(process.env.NEXT_PUBLIC_SHOW_DRAFTS || "");
+
   let filter: PostFilter = {};
 
   const today = new Date().toISOString();
