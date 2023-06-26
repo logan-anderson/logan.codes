cc2f244ac3263b512bda21e13fe3ed957ca8369a
Logan Anderson <landerson2@upei.ca>
2023-06-26 08:55:51 -0400

    update history page

--- pages/history.tsx
+++ pages/history.tsx
@@ -18,28 +18,40 @@ const HISTORY: {
     title: "Software Engineer",
     company: "Forestry.io / Tina CMS | Fulltime",
     date: "April 2020 - Present",
-    description: "",
-    icon: CodeBracketIcon,
-  },
-  {
-    title: "Web Developer / Machine Learning Engineer",
-    company: "IWave | Fulltime / Part Time (Co op)",
-    date: "April 2019 - April 2020",
-    description: "",
+    description:
+      "As an open source maintainer of TinaCMS, I built and maintained tools that enhance the developer experience by generating GraphQL schema and client from user configurations. Additionally, I actively supported and ensured the success of the community through Discord while working on implementing and maintaining features in a React Frontend, as well as backend features utilizing AWS services like DynamoDB, S3, and Serverless Stack (SST) for data caching, validation, and business logic.",
     icon: CodeBracketIcon,
   },
   {
     title: "Lab / Tutorial Instructor",
     company: "University of Prince Edward Island | Part Time",
-    date: "2020 - Present",
-    description: "",
+    date: "Sept 2020-April 2021",
+    description:
+      "I created engaging and informative Python practice problems, delivered them to students, and provided thoughtful and thorough explanations in response to their questions",
     icon: AcademicCapIcon,
+  },
+  {
+    title: "Web Developer / Machine Learning Engineer",
+    company: "Part Time",
+    date: "Sept 2019 - April 2020",
+    description:
+      "Played an integral role in building and fixing bugs on the Vue.js frontend, resulting in a seamless user experience",
+    icon: CodeBracketIcon,
+  },
+  {
+    title: "Data Science Specialist (Machine Learning)",
+    company: "iWave | Fulltime (Co op)",
+    date: "May 2019 - Aug 2019",
+    description:
+      "Utilized machine learning techniques and AWS to develop a document classification model, and successfully classified millions of documents by building a web scraper to extract data from specified websites",
+    icon: CodeBracketIcon,
   },
   {
     title: "Bachelor of Science in Computer Science (Honours)",
     company: "University of Prince Edward Island",
     date: "2017 - 2021",
-    description: "",
+    description:
+      "Double Major in Computer Science and Math, University of Prince Edward Island",
     icon: AcademicCapIcon,
   },
 ];
@@ -49,7 +61,7 @@ const WorkHistory = () => {
     <Layout preview={false} title="Work History">
       <div className="max-w-prose mx-auto">
         <ol className="relative border-l border-gray-200 dark:border-gray-700">
-          {HISTORY.map((item, index) => {
+          {HISTORY.map((item, _index) => {
             const Icon = item.icon;
             return (
               <li className="ml-6">
@@ -68,109 +80,6 @@ const WorkHistory = () => {
               </li>
             );
           })}
-          {/* <li className="mb-10 ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-          <svg
-          aria-hidden="true"
-          className="w-3 h-3 text-blue-800 dark:text-blue-300"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-          >
-          <path
-          fill-rule="evenodd"
-          d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-          clip-rule="evenodd"
-          ></path>
-          </svg>
-          </span>
-          <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-          Flowbite Application UI v2.0.0{" "}
-          <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">
-          Latest
-          </span>
-          </h3>
-          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-          Released on January 13th, 2022
-          </time>
-          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-          Get access to over 20+ pages including a dashboard layout, charts,
-          kanban board, calendar, and pre-order E-commerce & Marketing pages.
-          </p>
-          <a
-          href="#"
-          className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-          >
-          <svg
-          className="w-4 h-4 mr-2"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-          >
-              <path
-              fill-rule="evenodd"
-              d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
-              clip-rule="evenodd"
-              ></path>
-              </svg>{" "}
-              Download ZIP
-              </a>
-              </li>
-              <li className="mb-10 ml-6">
-              <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-              <svg
-              aria-hidden="true"
-              className="w-3 h-3 text-blue-800 dark:text-blue-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              >
-              <path
-              fill-rule="evenodd"
-              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-              clip-rule="evenodd"
-              ></path>
-              </svg>
-              </span>
-              <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-              Flowbite Figma v1.3.0
-              </h3>
-              <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              Released on December 7th, 2021
-              </time>
-              <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-              All of the pages and components are first designed in Figma and we
-              keep a parity between the two versions even as we update the
-              project.
-              </p>
-              </li>
-              <li className="ml-6">
-              <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-              <svg
-              aria-hidden="true"
-              className="w-3 h-3 text-blue-800 dark:text-blue-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              >
-              <path
-              fill-rule="evenodd"
-              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-              clip-rule="evenodd"
-              ></path>
-              </svg>
-              </span>
-              <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-              Flowbite Library v1.2.2
-              </h3>
-              <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              Released on December 2nd, 2021
-              </time>
-              <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-              Get started with dozens of web components and interactive elements
-              built on top of Tailwind CSS.
-              </p>
-            </li> */}
         </ol>
       </div>
     </Layout>
