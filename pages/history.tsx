b712c3ce2e1556062f0d3931aa904cc27ec60358
Logan Anderson <landerson2@upei.ca>
2023-06-25 22:04:48 -0400

    added basic ui for work history

---
+++ pages/history.tsx
@@ -0,0 +1,180 @@
+import { CodeBracketIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
+
+import Layout from "../components/layout/Layout";
+
+const HISTORY: {
+  title: string;
+  company: string;
+  date: string;
+  description: string;
+  icon: React.ForwardRefExoticComponent<
+    React.PropsWithoutRef<React.SVGProps<SVGSVGElement>> & {
+      title?: string;
+      titleId?: string;
+    } & React.RefAttributes<SVGSVGElement>
+  >;
+}[] = [
+  {
+    title: "Software Engineer",
+    company: "Forestry.io / Tina CMS | Fulltime",
+    date: "April 2020 - Present",
+    description: "",
+    icon: CodeBracketIcon,
+  },
+  {
+    title: "Web Developer / Machine Learning Engineer",
+    company: "IWave | Fulltime / Part Time (Co op)",
+    date: "April 2019 - April 2020",
+    description: "",
+    icon: CodeBracketIcon,
+  },
+  {
+    title: "Lab / Tutorial Instructor",
+    company: "University of Prince Edward Island | Part Time",
+    date: "2020 - Present",
+    description: "",
+    icon: AcademicCapIcon,
+  },
+  {
+    title: "Bachelor of Science in Computer Science (Honours)",
+    company: "University of Prince Edward Island",
+    date: "2017 - 2021",
+    description: "",
+    icon: AcademicCapIcon,
+  },
+];
+
+const WorkHistory = () => {
+  return (
+    <Layout preview={false} title="Work History">
+      <div className="max-w-prose mx-auto">
+        <ol className="relative border-l border-gray-200 dark:border-gray-700">
+          {HISTORY.map((item, index) => {
+            const Icon = item.icon;
+            return (
+              <li className="ml-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <Icon className="w-3 h-3 text-blue-800 dark:text-blue-300" />
+                </span>
+                <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                  {item.title}
+                </h3>
+                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  {item.company} | {item.date}
+                </time>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  {item.description}
+                </p>
+              </li>
+            );
+          })}
+          {/* <li className="mb-10 ml-6">
+          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+          <svg
+          aria-hidden="true"
+          className="w-3 h-3 text-blue-800 dark:text-blue-300"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+          >
+          <path
+          fill-rule="evenodd"
+          d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+          clip-rule="evenodd"
+          ></path>
+          </svg>
+          </span>
+          <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+          Flowbite Application UI v2.0.0{" "}
+          <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">
+          Latest
+          </span>
+          </h3>
+          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+          Released on January 13th, 2022
+          </time>
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          Get access to over 20+ pages including a dashboard layout, charts,
+          kanban board, calendar, and pre-order E-commerce & Marketing pages.
+          </p>
+          <a
+          href="#"
+          className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+          >
+          <svg
+          className="w-4 h-4 mr-2"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+          >
+              <path
+              fill-rule="evenodd"
+              d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
+              clip-rule="evenodd"
+              ></path>
+              </svg>{" "}
+              Download ZIP
+              </a>
+              </li>
+              <li className="mb-10 ml-6">
+              <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+              <svg
+              aria-hidden="true"
+              className="w-3 h-3 text-blue-800 dark:text-blue-300"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              >
+              <path
+              fill-rule="evenodd"
+              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+              clip-rule="evenodd"
+              ></path>
+              </svg>
+              </span>
+              <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+              Flowbite Figma v1.3.0
+              </h3>
+              <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+              Released on December 7th, 2021
+              </time>
+              <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+              All of the pages and components are first designed in Figma and we
+              keep a parity between the two versions even as we update the
+              project.
+              </p>
+              </li>
+              <li className="ml-6">
+              <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+              <svg
+              aria-hidden="true"
+              className="w-3 h-3 text-blue-800 dark:text-blue-300"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              >
+              <path
+              fill-rule="evenodd"
+              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+              clip-rule="evenodd"
+              ></path>
+              </svg>
+              </span>
+              <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+              Flowbite Library v1.2.2
+              </h3>
+              <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+              Released on December 2nd, 2021
+              </time>
+              <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+              Get started with dozens of web components and interactive elements
+              built on top of Tailwind CSS.
+              </p>
+            </li> */}
+        </ol>
+      </div>
+    </Layout>
+  );
+};
+
+export default WorkHistory;
